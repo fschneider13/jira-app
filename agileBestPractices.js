892bff9b293383e90a5be2a3c33409c8e96428a4
FABIO SCHNEIDER <38983036+fschneider13@users.noreply.github.com>
2025-07-12 21:28:08 +0200

    Add basic JIRA integration server

---
+++ agileBestPractices.js
@@ -0,0 +1,14 @@
+function getBestPractices() {
+  return [
+    'Maintain a prioritized backlog with clear user stories',
+    'Hold regular sprint planning and retrospectives',
+    'Keep work visible and transparent',
+    'Limit work in progress to improve focus',
+    'Use continuous integration and automated testing',
+    'Encourage collaborative code reviews',
+    'Deliver small, incremental improvements frequently',
+    'Gather feedback from stakeholders regularly'
+  ];
+}
+
+module.exports = { getBestPractices };
